d5da87220cd2351ce907d3d6370b86f013851f2d
Sreenivas harien Mone <sm2542@afsconnect2.njit.edu>
2017-12-08 17:58:37 -0500

    added login page

--- code/pages/homepage.php
+++ code/pages/homepage.php
@@ -5,13 +5,12 @@
     <meta charset="utf-8">
 
     <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
 
     <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>-->
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <![endif]-->
 </head>
 
@@ -22,7 +21,7 @@
     <?php
 
     //this how to print some data;
-    echo $data['site_name'];
+    //echo $data['site_name'];
 
     ?> </h1>
 
@@ -32,20 +31,27 @@
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit">Login</button>
-    </div>
-
+        <section id="content">
+            <form action="">
+                <h1>Login Form</h1>
+                <div>
+                    <input type="text" placeholder="Username" required="" id="username" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" required="" id="password" />
+                </div>
+                <div>
+                    <input type="submit" value="Log in" />
+                    <a href="index.php?page=accounts&action=register">Register</a>
+                </div>
+            </form><!-- form -->
+        </section><!-- content -->
+    </div><!-- container -->
 
 </form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
+<!--<h1><a href="index.php?page=accounts&action=register">Register</a></h1>-->
+<!--<script src="js/scripts.js"></script>-->
+<script  src="js/index.js"></script>
 
-
-<script src="js/scripts.js"></script>
 </body>
 </html>
