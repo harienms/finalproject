452120ff34ddd2b2e57d4e6ea681dc5b91f1e420
Sreenivas harien Mone <sm2542@afsaccess1.njit.edu>
2017-12-17 17:43:50 -0500

    upadate page

--- code/pages/edit_task.php
+++ code/pages/edit_task.php
@@ -1,6 +1,62 @@
-put a page with in a form to edit a task and have a delete button in another form to delete
+<!doctype html>
 
-<form action="index.php?page=tasks&action=delete&id=<?php $data['id'] ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Crete Task</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+
+</head>
+
+<body>
+
+<form action="index.php?page=tasks&action=edipt&id=<?php echo $data->id; ?>" method="post">
+
+    <div class="container">
+        <section id="content">
+            <form action="">
+                <h1>Update task</h1>
+
+                <div>
+                    <label><b>ID: <?php echo $_GET['id']; ?></b></label><br><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </div><br><br><br>
+
+                <div>
+                    <input type="text" name = "createddate" placeholder="Create Date" required="" id="createddate" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </div><br><br><br>
+
+                <div>
+                    <input type="text" name = "duedate" placeholder="Due Date" required="" id="duedate" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </div><br><br><br>
+                <div>
+                    <input type="text" name = "message" placeholder="Message" required="" id="message" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </div><br><br><br>
+                <div>
+                    <input type="text" name = "isdone" placeholder="IS Done" required="" id="isdone" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </div><br><br><br>
+
+                <div>
+                    <input type="submit" value="Update" />
+                    &nbsp;
+                </div>
+                <br><br>
+            </form><!-- form -->
+        </section><!-- content -->
+    </div><!-- container -->
 </form>
 
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
--- code/pages/show_task.php
+++ code/pages/show_task.php
@@ -8,28 +8,37 @@
     <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
 </head>
 
 <body>
 
+<h1 align="left">USER TASK</h1>
+<br>
+
+
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
 
 print utility\htmlTable::generateTableFromOneRecord($data);
+
 ?>
 
-<form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
-</form>
+    <h2><a href="index.php?page=tasks&action=edit&id=<?php echo $data->id; ?>">Update Task</a></h2>
 
+    <br>
 
+    <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2">
+        <button type="submit" form="form2" value="delete">Delete Task</button>
+    </form>
 
+    <br>
 
-<script src="js/scripts.js"></script>
+    <h2><label><a href="index.php">Logout</a></label>
+
+        <script src="js/scripts.js"></script>
 </body>
 </html>
